764b34e446001f88926b6919f9e07f56491df0b5
Jon Lee <jonlee836@gmail.com>
2016-11-24 05:56:45 -0800

    test

--- app/src/main/java/com/example/nonroot/libertyprime/MainActivity.java
+++ app/src/main/java/com/example/nonroot/libertyprime/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.nonroot.libertyprime;
 
+import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,133 +14,133 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import java.lang.reflect.Field;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener{
+public class MainActivity extends AppCompatActivity{
 
     Boolean GIVE_ME_LIBERTY_OR_GIVE_ME_DEATH = Boolean.TRUE;
 
-    int AD_VICTORIUM, COMMONWEALTH;
+    int AD_VICTORIAM, PRICE_OF_FREEDOM, BRITISH_TYRANNY;
 
-    public int ARRAY_OF_DEMOCRACY[];
-    public String ARRAY_OF_LIBERTY[];
-    public ListView ARRAY_OF_FREEDOM;
+    int ONE_IF_BY_LAND[];
+    int TWO_IF_BY_SEA[];
 
-    public ImageView REVOLUTION;
-    public AudioManager LIVE_FREE_OR_DIE;
-    public TextView SHALL_NOT_BE_INFRINGED;
+    int ARRAY_OF_DEMOCRACY[];
 
-    public MediaPlayer LIBERTY_BELL, BETTER_DEAD_THAN_RED;
+    String ARRAY_OF_LIBERTY[];
+    ListView ARRAY_OF_FREEDOM;
+    TextView SHALL_NOT_BE_INFRINGED;
 
-    MainActivity(){
-        DECLARE_INDEPENDENCE();
-    }
+    AudioManager LIVE_FREE_OR_DIE;
+    Field UNJUST_TAXATION[];
+    MediaPlayer LIBERTY_BELL, OLD_GLORY;
+    ImageView USAUSAUSA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DECLARE_INDEPENDENCE();
+
+        USAUSAUSA = (ImageView) findViewById(R.id.USAUSAUSA);
         ARRAY_OF_FREEDOM = (ListView) findViewById(R.id.audioList);
-        REVOLUTION = (ImageView) findViewById(R.id.REVOLUTION);
         SHALL_NOT_BE_INFRINGED = (TextView) findViewById(R.id.topLabel_ADVICTORIUM);
 
         LIVE_FREE_OR_DIE = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        //LIVE_FREE_OR_DIE.setStreamVolume(AudioManager.STREAM_MUSIC, 100, 0);
+        LIVE_FREE_OR_DIE.setStreamVolume(AudioManager.STREAM_MUSIC, 100, 0);
 
         final ArrayAdapter LIBERTY = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, ARRAY_OF_LIBERTY);
         ARRAY_OF_FREEDOM.setAdapter(LIBERTY);
 
         LIBERTY_BELL = new MediaPlayer();
-        BETTER_DEAD_THAN_RED = new MediaPlayer();
-
-        initMusicPlayer();
+        OLD_GLORY = new MediaPlayer();
 
         ARRAY_OF_FREEDOM.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-                BRANCHES();
+                SUCCESSION();
+                COLONIES(ARRAY_OF_DEMOCRACY[position]);
 
-                COMMONWEALTH = ARRAY_OF_DEMOCRACY[position];
-
-                LIBERTY_BELL.reset();
-                LIBERTY_BELL = playVoice(ARRAY_OF_DEMOCRACY[position]);
-                LIBERTY_BELL.start();
+                BEFORE_1776();
+                A_GIANT_MISTAKE();
             }
         });
 
         SHALL_NOT_BE_INFRINGED.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BRANCHES();
+                SUCCESSION();
+                UNALIENABLE_RIGHTS();
 
-                BETTER_DEAD_THAN_RED.reset();
-                BETTER_DEAD_THAN_RED = playVoice(AD_VICTORIUM);
-                BETTER_DEAD_THAN_RED.start();
+                BEFORE_1776();
+                A_GIANT_MISTAKE();
             }
         });
 
-        // play primemarch along with primeaudio playing at random
-        REVOLUTION.setOnClickListener(new View.OnClickListener(){
+        USAUSAUSA.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 AFTER_1776();
-                CHECKS_AND_BALANCES();
+                BALANCE_OF_POWER();
 
-                if (!BETTER_DEAD_THAN_RED.isLooping()){
-                    BETTER_DEAD_THAN_RED.setLooping(Boolean.TRUE);
-                }
-
-                BETTER_DEAD_THAN_RED = playVoice(AD_VICTORIUM);
-                BETTER_DEAD_THAN_RED.start();
-
-                if(!LIBERTY_BELL.isPlaying()){
+                if (!LIBERTY_BELL.isPlaying() && GIVE_ME_LIBERTY_OR_GIVE_ME_DEATH){
                     LET_FREEDOM_RING();
                 }
             }
         });
     }
 
-    @Override
-    public void onCompletion(MediaPlayer mp) {
-        //check if song playing
-        if(GIVE_ME_LIBERTY_OR_GIVE_ME_DEATH){
-            int rndaudio = new Random().nextInt(ARRAY_OF_DEMOCRACY.length);
-            LIBERTY_BELL.reset();
-            LIBERTY_BELL = playVoice(ARRAY_OF_DEMOCRACY[rndaudio]);
-            LIBERTY_BELL.start();
-        }
-    }
-
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-
-    }
-
-    public void DEATH_BEFORE_T
-
-    public MediaPlayer playVoice(int rawID){
-        MediaPlayer a = MediaPlayer.create(this, rawID);
-        a.setVolume(0.9f, 0.9f);
-
-        return a;
-    }
-
     public void LET_FREEDOM_RING(){
 
-        LIBERTY_BELL.reset();
-        if (!LIBERTY_BELL.isPlaying()) {
-            int rndaudio = new Random().nextInt(ARRAY_OF_DEMOCRACY.length);
-            LIBERTY_BELL = playVoice(ARRAY_OF_DEMOCRACY[rndaudio]);
-            LIBERTY_BELL.start();
+        int currRnd;
+        do{
+            currRnd = new Random().nextInt(ARRAY_OF_DEMOCRACY.length);
+        }while(currRnd == PRICE_OF_FREEDOM);
+
+        PRICE_OF_FREEDOM = currRnd;
+
+        LIBERTY_BELL = MediaPlayer.create(this, ARRAY_OF_DEMOCRACY[currRnd]);
+        LIBERTY_BELL.start();
+
+        // recursively keep playing until bool is set false
+        if (currRnd < BRITISH_TYRANNY) {
+            LIBERTY_BELL.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    LET_FREEDOM_RING();
+                }
+            });
         }
 
+        UNALIENABLE_RIGHTS();
     }
 
-    public void BRANCHES(){
+    public void UNALIENABLE_RIGHTS(){
+        OLD_GLORY.reset();
+        OLD_GLORY = MediaPlayer.create(this, AD_VICTORIAM);
+        OLD_GLORY.start();
+    }
+
+    public void COLONIES(int rawID){
+        try{
+            LIBERTY_BELL.reset();
+            LIBERTY_BELL = MediaPlayer.create(this, rawID);
+            LIBERTY_BELL.start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void SUCCESSION(){
         if (GIVE_ME_LIBERTY_OR_GIVE_ME_DEATH == Boolean.TRUE){
-            CHECKS_AND_BALANCES();
+            LIBERTY_BELL.reset();
+            OLD_GLORY.reset();
+
             BEFORE_1776();
         }
     }
@@ -147,71 +149,103 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         GIVE_ME_LIBERTY_OR_GIVE_ME_DEATH = Boolean.FALSE;
     }
 
+    public void A_GIANT_MISTAKE(){
+
+    }
+
     public void AFTER_1776(){
         GIVE_ME_LIBERTY_OR_GIVE_ME_DEATH = Boolean.TRUE;
     }
 
-    public void CHECKS_AND_BALANCES(){
+    public void BALANCE_OF_POWER(){
         LIBERTY_BELL.reset();
-        BETTER_DEAD_THAN_RED.reset();
-    }
-
-    public void initMusicPlayer(){
-        LIBERTY_BELL.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        LIBERTY_BELL.setOnPreparedListener((MediaPlayer.OnPreparedListener) this);
-        LIBERTY_BELL.setOnCompletionListener((MediaPlayer.OnCompletionListener) this);
-        LIBERTY_BELL.setOnErrorListener((MediaPlayer.OnErrorListener) this);
-
-        BETTER_DEAD_THAN_RED.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        BETTER_DEAD_THAN_RED.setOnPreparedListener((MediaPlayer.OnPreparedListener) this);
-        BETTER_DEAD_THAN_RED.setOnCompletionListener((MediaPlayer.OnCompletionListener) this);
-        BETTER_DEAD_THAN_RED.setOnErrorListener((MediaPlayer.OnErrorListener) this);
+        OLD_GLORY.reset();
     }
 
     public void DECLARE_INDEPENDENCE() {
 
-        Field fields[] = R.raw.class.getDeclaredFields();
+        UNJUST_TAXATION = R.raw.class.getDeclaredFields();
 
-        int flen = fields.length;
         int startIndstr = 0;
+        int mrchIndx_Start = -1;
+        int weapIndx_Start = -1;
 
-        // A "$change" string will appear if you're using the instant run feature.
-        // bug? 11/16/2016 : Found a string at Field[49] named "SerialVersionUID" which had an R.raw
+        // Bug? A "$change" string will appear if you're using the instant run feature.
+        // 11/16/2016 : Found a string at Field[49] named "SerialVersionUID" which had an R.raw
         // value of 0.
 
+        String md = "z_p";
+        String wd = "z_w";
+
         try {
-            for (int i = 0; i < fields.length; i++) {
-                if (!BadStr_check(fields[i].getName())) {
-                    flen -= 1;
+            for (int i = 0; i < UNJUST_TAXATION.length; i++) {
+                String str_name = UNJUST_TAXATION[i].getName();
+
+                if (BadStr_check(str_name)){
+                    BRITISH_TYRANNY++;
                 }
-                if (fields[i].getName() == "primemarch") {
-                    AD_VICTORIUM = fields[i].getInt(null);
+                if (Type_Check(str_name, md) == Boolean.TRUE && mrchIndx_Start == -1){
+                    mrchIndx_Start = i;
+                    System.out.println("mrchindx : " + mrchIndx_Start);
+                }
+                if (Type_Check(str_name, wd) == Boolean.TRUE && weapIndx_Start == -1){
+                    weapIndx_Start = i;
+                    System.out.println("weapindx : " + weapIndx_Start);
                 }
 
+                if (UNJUST_TAXATION[i].getName() == "z_primemarch") {
+                    AD_VICTORIAM = UNJUST_TAXATION[i].getInt(null);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        ARRAY_OF_DEMOCRACY = new int[flen];
-        ARRAY_OF_LIBERTY = new String[flen];
+        // marching audio, weapon audio
+        ONE_IF_BY_LAND = new int[weapIndx_Start - mrchIndx_Start];
+        TWO_IF_BY_SEA = new int[UNJUST_TAXATION.length - weapIndx_Start];
 
+        int foo = weapIndx_Start-mrchIndx_Start;
+        int bar = UNJUST_TAXATION.length - weapIndx_Start;
+
+        System.out.println("one if by land size, index : " + foo + " " + mrchIndx_Start);
+        System.out.println("two if by sea size, index : " + bar + " " + weapIndx_Start);
+
+        // audio int rawIDs and strings for viewlist
+        ARRAY_OF_DEMOCRACY = new int[BRITISH_TYRANNY];
+        ARRAY_OF_LIBERTY = new String[BRITISH_TYRANNY];
+
+        // 1 min clips of prime marching
         try {
-            for( int i = 0; i < fields.length; i++) {
 
-                if (!BadStr_check(fields[i].getName())){
+            int mrchCount = 0;
+            int weapCount = 0;
+
+            for(int i = 0; i < UNJUST_TAXATION.length; i++) {
+
+                if (!BadStr_check(UNJUST_TAXATION[i].getName()) && i < mrchIndx_Start){
                     continue;
                 }
+                if (i < mrchIndx_Start){
+                    ARRAY_OF_LIBERTY[startIndstr] = UNJUST_TAXATION[i].getName();
+                    ARRAY_OF_LIBERTY[startIndstr] = ARRAY_OF_LIBERTY[startIndstr].toUpperCase();
 
-                ARRAY_OF_LIBERTY[startIndstr] = fields[i].getName();
-                ARRAY_OF_LIBERTY[startIndstr] = ARRAY_OF_LIBERTY[startIndstr].toUpperCase();
-
-                if (ARRAY_OF_LIBERTY[startIndstr].equals("COMMUNISM___")){
-                    ARRAY_OF_LIBERTY[startIndstr] = "SOCIALISM";
+                    if (ARRAY_OF_LIBERTY[startIndstr].equals("COMMUNISM___")){
+                        ARRAY_OF_LIBERTY[startIndstr] = "SOCIALISM";
+                    }
+                    ARRAY_OF_LIBERTY[startIndstr] = ARRAY_OF_LIBERTY[startIndstr].replace("_", " ");
+                    ARRAY_OF_DEMOCRACY[startIndstr] = UNJUST_TAXATION[i].getInt(null);
                 }
 
-                ARRAY_OF_LIBERTY[startIndstr] = ARRAY_OF_LIBERTY[startIndstr].replace("_", " ");
-                ARRAY_OF_DEMOCRACY[startIndstr] = fields[i].getInt(null);
+
+                if (i >= mrchIndx_Start && i < weapIndx_Start) {
+                    ONE_IF_BY_LAND[mrchCount] = UNJUST_TAXATION[i].getInt(null);
+                    mrchCount++;
+                }
+                else if (i >= weapIndx_Start && i < UNJUST_TAXATION.length) {
+                    TWO_IF_BY_SEA[weapCount] = UNJUST_TAXATION[i].getInt(null);
+                    weapCount++;
+                }
 
                 System.out.println(ARRAY_OF_LIBERTY[startIndstr] + " " + startIndstr + " " + ARRAY_OF_DEMOCRACY[startIndstr]);
 
@@ -221,21 +255,31 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
             e.printStackTrace();
         }
     }
-
+// test 
     private Boolean BadStr_check(String a){
 
-        // for some reason there's random crap that is generated when you fill
+        // this gets rid of the random crap that is generated when you fill
         // a Field object made from R.raw.class.getDecalredFields
 
         // primemarch is not one of them.
 
         if (a == "$change" ||
-            a == "primemarch" ||
             a == "serialVersionUID") {
+
             return Boolean.FALSE;
         }
         else {
             return Boolean.TRUE;
         }
+    }
+
+    private Boolean Type_Check(String a, String b){
+        String c = a.substring(0, Math.min(a.length(), b.length()));
+
+        if (a.contains(b)) {
+            System.out.println("MATCH     " + a + "   " + b);
+            return true;
+        }
+        return false;
     }
 }
